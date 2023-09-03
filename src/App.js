74b9c4e2a0052510a8a3823afc0ae1e7a3cfe649
PrathamSakhare <psakhre@themohh.com>
2023-09-03 16:01:53 +0530

    made login and signup page and settled up routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-import ProductList from './features/product-list/ProductList';
-import { Home } from './pages/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import ProductList from "./features/product-list/ProductList";
+import { Home } from "./pages/HomePage";
+import Login from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
